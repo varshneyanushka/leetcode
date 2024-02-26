46feb38a6f10a6e8ed290c68bcea27f1282a85bb
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-02-27 00:09:05 +0530

    Time: 2 ms (51.47%) | Memory: 7.1 MB (39.89%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int getSum(int a, int b) {
+        if(a==0)
+        {
+            return b;
+        }
         while(b!=0)
         {
             int temp=(a & b)<<1;
