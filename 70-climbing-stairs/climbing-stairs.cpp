91767946b1a9f8ca6475d4387928eddd0dd4bf4b
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-03-02 15:28:24 +0530

    Time: 2 ms (47.05%) | Memory: 7.1 MB (25.38%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int climbStairs(int n) {
-      int one=1;
-      int two=1;
-      n=n-1;
-      while(n--)
-      {
-          int temp=one;
-          one =one +two;
-          two=temp;
-      }
-        return one;
-    }
+        int prev=1;
+        if(n==1)
+        {
+            return 1;
+        }
+        int ans=2;
+        for(int i=0;i<n-2;i++)
+        {
+            int temp =ans;
+           ans=ans+prev;
+           prev=temp;
 
+        }
+        return ans;
+    }
 };
