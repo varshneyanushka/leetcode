aa6d1721b63d01beec2616f51e3bbe24f5c275c7
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-05-17 23:55:03 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (95.28%) - LeetSync

---
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+         vector<bool> ans;
+        for(int i=0;i<candies.size();i++)
+        {
+            bool x=false;
+            for(int j=0;j<candies.size();j++)
+            {
+                if(candies[i]+extraCandies>=candies[j])
+                {
+                    x=true;
+                }
+                else
+                {
+                    x=false;
+                    break;
+                }
+               
+            }
+            ans.push_back(x);
+        }
+        return ans;
+        
+    }
+};
