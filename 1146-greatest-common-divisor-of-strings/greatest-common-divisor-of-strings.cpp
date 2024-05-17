8dc6efa898e1382b624bf34670b58372737f664a
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-05-17 23:15:52 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (30.37%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    string gcdOfStrings(string s1, string s2) {
+        int n=s1.length();
+        int m=s2.length();
+        int c=gcd(n,m);
+        if(s1+s2 != s2+s1)
+        {
+            return "";        }
+         if(s1.substr(0,c)==s2.substr(0,c))
+         {
+            return s1.substr(0,c);
+         }   
+         else{
+            return "";
+         }
+      
+    }
+     int gcd(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+};
