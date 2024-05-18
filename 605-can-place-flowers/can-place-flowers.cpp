3eb1829af9a10e29aa65f974ba80c4dd46da95fa
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-05-18 14:03:22 +0530

    Time: 8 ms (82.58%) | Memory: 24.4 MB (13.55%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -4,39 +4,25 @@ public:
         int ans=0;
         vector<int> f;
         f=flowers;
-        
-        for(int i=0;i<f.size();i++)
+        f.insert(f.begin(),0);
+        f.push_back(0);
+        for(int i=1;i<f.size()-1;i++)
         {
-            if(f[i]==0){
-                if(f.size()==1){
-                    ans++;
-                    f[i]=1;
-                } else {
-                    if(i==0) {
-                        if(f[i+1]==0){
-                            ans++;
-                            f[i]=1;
-                        }
-                    }
-                    else if(i==f.size()-1 ){
-                            if(f[i-1]==0){
-                                ans++;
-                                f[i]=1;
-                            }
-                    } else if(f[i-1]==0 && f[i+1]==0){
-                        ans++;
-                        f[i]=1;
-                    }
-
-                }
+            
+            if(f[i-1]==0 && f[i]==0 && f[i+1]==0)
+            {
+                f[i]=1;
+                ans++;
             }
             
         }
+        if(ans>=n){
+                return true;
+            }
+            else{
+                     return false;
+            }
         
-        if(n <= ans){
-            return true;
-        }else {
-            return false;
-        }
+       
     }
 };
