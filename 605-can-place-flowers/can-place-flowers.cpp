05448e6d1758730b0879e96ea98ed4de9b675452
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-05-18 14:25:58 +0530

    Time: 7 ms (93.18%) | Memory: 22.8 MB (53.18%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowers, int n) {
+    bool canPlaceFlowers(vector<int>& f, int n) {
         int ans=0;
-        vector<int> f;
-        f=flowers;
         f.insert(f.begin(),0);
         f.push_back(0);
         for(int i=1;i<f.size()-1;i++)
