540d592c934891f526dd079b31ccf507df68be80
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-05-28 02:18:56 +0530

    Time: 16 ms (86.98%) | Memory: 11.3 MB (39.72%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -4,10 +4,10 @@ public:
         int ans=INT_MIN;
         int l=0; int n=s.length();int r=k;
         int curr=0;
-        string temp=s;
+     
         for(int i=0;i<k;i++)
         {
-            if(temp[i]=='a'||temp[i]=='e'||temp[i]=='i'||temp[i]=='o'||temp[i]=='u')
+            if(s[i]=='a'||s[i]=='e'||s[i]=='i'||s[i]=='o'||s[i]=='u')
             {
                 curr++;
             }
@@ -15,12 +15,12 @@ public:
         ans=curr;
         while(r<n)
         {   
-            if(temp[l]=='a'||temp[l]=='e'||temp[l]=='i'||temp[l]=='o'||temp[l]=='u')
+            if(s[l]=='a'||s[l]=='e'||s[l]=='i'||s[l]=='o'||s[l]=='u')
             {
                 curr--;
             }
             l++;
-            if(temp[r]=='a'||temp[r]=='e'||temp[r]=='i'||temp[r]=='o'||temp[r]=='u')
+            if(s[r]=='a'||s[r]=='e'||s[r]=='i'||s[r]=='o'||s[r]=='u')
             {
                 curr++;
             }
