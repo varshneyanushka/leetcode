a1d7d8aa1703e187fe672d015380011936e03641
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-05-30 00:57:36 +0530

    Time: 3 ms (48.57%) | Memory: 7.9 MB (68.02%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans; // Store the results
+    vector<vector<int>> ans; 
     vector<int> t;
     void backtrack( int k, int n, int j) {
         if (n == 0 && k == 0) {
             ans.push_back(t);
-           // t.clear(); // Found a valid combination
+           
             return;
         }
-        if (n < 0 || k < 0) return; // Not a valid path
+        if (n < 0 || k < 0) return;
 
         for (int i = j; i <= 9; i++) {
-            t.push_back(i); // Choose the current number
-            backtrack( k - 1, n - i, i + 1); // Recurse with updated parameters
-            t.pop_back(); // Undo the choice
+            t.push_back(i); 
+            backtrack( k - 1, n - i, i + 1); 
+            t.pop_back(); 
         }
     }
 
