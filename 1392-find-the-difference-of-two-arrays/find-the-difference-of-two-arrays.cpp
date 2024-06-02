82159a8bf6b135e514c7ee0e047b68f7692340bf
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-06-02 11:35:11 +0530

    Time: 89 ms (6.85%) | Memory: 29.9 MB (94.74%) - LeetSync

---
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    vector<vector<int>> findDifference(vector<int>& nums1, vector<int>& nums2) {
+
+        vector<vector<int>> ans(2);
+        int n=nums1.size();
+        int m=nums2.size();
+        for(int r:nums1)
+        {
+            bool flag=false;
+            for(int w:nums2)
+            {
+                if(r==w)
+                {
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag)
+            {
+                ans[0].push_back(r);
+            }
+        }
+
+        for(int r:nums2)
+        {
+            bool flag=false;
+            for(int w:nums1)
+            {
+                if(r==w)
+                {
+                    flag=true;
+                    break;
+                }
+            }
+            if(!flag)
+            {
+                ans[1].push_back(r);
+            }
+        }
+        sort(ans[0].begin(), ans[0].end());   
+        auto last = unique(ans[0].begin(), ans[0].end());  
+        ans[0].erase(last, ans[0].end());  
+
+        sort(ans[1].begin(), ans[1].end());   
+        auto las = unique(ans[1].begin(), ans[1].end());  
+        ans[1].erase(las, ans[1].end());  
+    
+
+        return ans;
+        
+    }
+};
