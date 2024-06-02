5564914d5fe3d0d32bb0c4ea93d67f7f3dcccaae
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-06-02 23:20:46 +0530

    Time: 124 ms (17.61%) | Memory: 23.4 MB (16.71%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -44,15 +44,15 @@ public:
          sort(c1.begin(),c1.end());
         sort(c2.begin(),c2.end());
 
-        for(int i=0;i<v1.size();i++)
-        {
-            cout<<v1[i];
-        }
-        cout<<endl;
-        for(int i=0;i<v2.size();i++)
-        {
-            cout<<v2[i];
-        }
+        // for(int i=0;i<v1.size();i++)
+        // {
+        //     cout<<v1[i];
+        // }
+        // cout<<endl;
+        // for(int i=0;i<v2.size();i++)
+        // {
+        //     cout<<v2[i];
+        // }
         
         if(v1==v2 && c1==c2)
         {
