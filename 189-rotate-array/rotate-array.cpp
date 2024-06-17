c78f77f01c887743c05951a6bbce39bc911cd5ef
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-06-18 00:20:15 +0530

    Time: 18 ms (77.83%) | Memory: 29.2 MB (5.58%) - LeetSync

---
+++ 189-rotate-array/rotate-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        if(n==1)
+        {
+            return;
+        }
+        while(n<k)
+        {
+            k=k-n;
+            
+        }
+        
+        vector<int> temp={nums.begin()+(n-k),nums.end()};
+        temp.insert(temp.end(),nums.begin(),nums.end());
+        nums={temp.begin(),temp.end()-k};
+        
+    }
+};
