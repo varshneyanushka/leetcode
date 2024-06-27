f518da85ebc522c3d48984ac33732a7e1aecd076
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-06-27 17:22:55 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (36.13%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int maxProfit(vector<int> p) {
+        int n=p.size();
+        int ans=0;
+        int b=p[0];
+         int curr=0;
+        for(int i=1;i<n;i++)
+        {
+           
+            if(p[i-1]>p[i])
+            {b=p[i];
+            ans+=curr;
+            curr=0;
+            }
+            
+            if(b<p[i])
+            {
+                cout<<curr<<endl;
+                curr=max(p[i]-b,curr);
+                
+
+            }
+
+
+            
+        }
+       ans+=curr;
+        return ans;
+        
+    }
+};
