bdbe6c486509ab4ea85b1ab8686232464d031622
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-07-05 13:41:23 +0530

    Time: 16 ms (32.84%) | Memory: 23.2 MB (5.61%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// get all node values in a vector and sort it to get the kth smallest value
 class Solution {
 public:
     vector<int> v;
