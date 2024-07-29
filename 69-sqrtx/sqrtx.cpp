8ed0d603708d6f58259526d6f02e43e5d227d637
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-07-29 22:05:23 +0530

    Time: 5 ms (34.55%) | Memory: 8.3 MB (18.43%) - LeetSync

--- 69-sqrtx/sqrtx.cpp
+++ 69-sqrtx/sqrtx.cpp
@@ -2,11 +2,35 @@ class Solution {
 public:
     int mySqrt(int x) {
         if(x==0||x==1){return x;}
-        long long int num=1;
-       while(num*num<=x)
-       {
-            num++;
-       }
-       return num-1;
+
+        //defining starting range
+        int s=0;
+        int e=x;
+        
+        long long int mid;
+        
+        while(s<e )
+        {
+             mid= (s+e)/2;
+            if(mid*mid>x)
+            {
+                
+                e=mid;
+            }
+            else if(mid*mid<x)
+            {
+              s=mid;
+            }
+            else if(mid*mid==x)
+            {
+                return mid;
+            }
+            if(mid*mid<x && (mid+1)*(mid+1)>x)
+            {   return mid;
+            }
+            
+        }
+        return mid;
+
     }
 };
