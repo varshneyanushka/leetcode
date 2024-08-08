0f8187e6d2f5609bb17fde007a6fae7377709c3a
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2024-08-08 09:34:54 +0530

    Time: 6 ms (71.14%) | Memory: 12.8 MB (43.97%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -16,7 +16,7 @@ for (int i = 0; i <= n; i++) {
         {
             for(int j=n-1;j>=0;j--)
             {
-                cout<<dp[i][j];
+               
                 if(word2[i]==word1[j])
                 {
                     dp[i][j]=dp[i+1][j+1];
@@ -26,7 +26,7 @@ for (int i = 0; i <= n; i++) {
                     dp[i][j]=1+min(dp[i+1][j+1],min(dp[i+1][j],dp[i][j+1]));
                 }
             }
-            cout<<endl;
+           
         }
         return dp[0][0];
     }
