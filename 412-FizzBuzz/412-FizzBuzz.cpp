c46ffe92b2afa7a838150c1d6c34f2b36a77d3cf
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:57:30 +0530

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.cpp
@@ -0,0 +1,14 @@
+// Last updated: 07/11/2025, 19:57:30
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> answer ;
+        for (int i=1; i<=n; i++){
+            if ((i%3==0) && (i%5==0)) answer.push_back("FizzBuzz") ;
+            else if (i%3==0) answer.push_back("Fizz") ;
+            else if (i%5==0) answer.push_back("Buzz") ;
+            else answer.push_back(to_string(i)) ;
+        }
+        return answer ;
+    }
+};
