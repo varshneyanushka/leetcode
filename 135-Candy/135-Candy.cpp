d7a2a69cde7b5cf63ad7afc19db7f31ff6ead96c
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:58:20 +0530

    Create 135-Candy

---
+++ 135-Candy/135-Candy.cpp
@@ -0,0 +1,41 @@
+// Last updated: 07/11/2025, 19:58:20
+class Solution {
+public:
+    int candy(vector<int> a) {
+        int n=a.size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n-1;i++)
+        {
+            if(a[i]<a[i+1])
+            {
+                dp[i+1]=dp[i]+1;
+            }
+            else if(a[i]>a[i+1] && dp[i]<=dp[i+1])
+            {
+                dp[i]++;
+            }
+            
+            
+        }
+      
+        for(int i=n-1;i>0;i--)
+        {
+            if(dp[i]==dp[i-1] && a[i]!=a[i-1])
+            {
+                dp[i-1]++;
+            }
+            if(a[i-1]>a[i] && dp[i-1]<dp[i])
+            {
+                dp[i-1]=dp[i]+1;
+            }
+        }
+        int sum=0;
+        for(int i=0;i<n;i++)
+        {
+           
+            sum=sum+dp[i];
+        }
+
+        return sum;
+    }
+};
