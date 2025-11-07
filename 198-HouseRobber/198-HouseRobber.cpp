30e3a583e519b9bacae3e5fe42a0df4195d9cdda
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:58:09 +0530

    Create 198-HouseRobber

---
+++ 198-HouseRobber/198-HouseRobber.cpp
@@ -0,0 +1,23 @@
+// Last updated: 07/11/2025, 19:58:09
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> dp(n,0);
+        if(n==0)
+        {
+            return 0;
+        }
+        if(n==1)
+        {
+            return nums[0];
+        }
+        dp[0]=nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        for(int i=2;i<n;i++)
+        {
+            dp[i]=max(dp[i-1],dp[i-2]+nums[i]);
+        }
+        return dp[n-1];
+    }
+};
