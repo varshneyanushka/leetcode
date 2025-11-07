01f022874dc3f147b69bac830b0968c74d2b9941
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:57:44 +0530

    Create 338-CountingBits

---
+++ 338-CountingBits/338-CountingBits.cpp
@@ -0,0 +1,15 @@
+// Last updated: 07/11/2025, 19:57:43
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n + 1, 0);
+        int m = 1;
+        for (int i = 1; i <= n; i++) {
+            if (m * 2 == i) {
+                m = i;
+            }
+            ans[i] = 1 + ans[i - m];
+        }
+        return ans;
+    }
+};
