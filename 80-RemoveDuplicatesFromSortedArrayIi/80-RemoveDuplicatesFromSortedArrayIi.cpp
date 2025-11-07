ae976d76fc44f03bbf5665a31c9747a7b7e94c24
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:58:46 +0530

    Create 80-RemoveDuplicatesFromSortedArrayIi

---
+++ 80-RemoveDuplicatesFromSortedArrayIi/80-RemoveDuplicatesFromSortedArrayIi.cpp
@@ -0,0 +1,32 @@
+// Last updated: 07/11/2025, 19:58:46
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j=1;
+        int count=1;
+        int n=nums.size();
+        int k=1;
+        for(int i=1;i<nums.size();i++)
+        {
+            if(nums[i]==nums[i-1] && count==1)
+            {
+                nums[j]=nums[i];
+                j++;
+                k++;
+                count++;
+            }
+            else if(nums[i]==nums[i-1] && count>=2)
+            {
+                count++;
+            }
+            else if(nums[i]!=nums[i-1])
+            {
+                nums[j]=nums[i];
+                count=1;
+                k++;
+                j++;
+            }
+        }
+            return k;
+    }
+};
