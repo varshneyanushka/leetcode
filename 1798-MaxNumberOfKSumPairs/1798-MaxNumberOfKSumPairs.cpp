8bee96b43f5607bd09228136087684c4879c3105
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:56:48 +0530

    Create 1798-MaxNumberOfKSumPairs

---
+++ 1798-MaxNumberOfKSumPairs/1798-MaxNumberOfKSumPairs.cpp
@@ -0,0 +1,22 @@
+// Last updated: 07/11/2025, 19:56:48
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int ans=0;
+        unordered_map<int, int> f;
+        for(int num:nums){
+            int complement=k-num;
+            if(f[complement]>0)
+            {
+                ans++;
+                f[complement]--;
+            }
+            else
+            {
+                f[num]++;
+            }
+        }
+        return ans;
+
+    }
+};
