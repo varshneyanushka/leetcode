a41fb118b4acacb36cd772ec2097b94bed717bfe
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:58:02 +0530

    Create 216-CombinationSumIii

---
+++ 216-CombinationSumIii/216-CombinationSumIii.cpp
@@ -0,0 +1,26 @@
+// Last updated: 07/11/2025, 19:58:01
+class Solution {
+public:
+    vector<vector<int>> ans; 
+    vector<int> t;
+    void backtrack( int k, int n, int j) {
+        if (n == 0 && k == 0) {
+            ans.push_back(t);
+           
+            return;
+        }
+        if (n < 0 || k < 0) return;
+
+        for (int i = j; i <= 9; i++) {
+            t.push_back(i); 
+            backtrack( k - 1, n - i, i + 1); 
+            t.pop_back(); 
+        }
+    }
+
+    vector<vector<int>> combinationSum3(int k, int n) {
+        
+        backtrack( k, n, 1);
+        return ans;
+    }
+};
