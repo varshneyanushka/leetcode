dd486e7238f454300d9b47ceee4e830f48a2cd61
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:58:24 +0530

    Create 129-SumRootToLeafNumbers

---
+++ 129-SumRootToLeafNumbers/129-SumRootToLeafNumbers.cpp
@@ -0,0 +1,22 @@
+// Last updated: 07/11/2025, 19:58:23
+class Solution {
+public:
+    int sumNumbers(TreeNode* root) {
+        return dfs(root, 0);        
+    }
+
+private:
+    int dfs(TreeNode* node, int num) {
+        if (node == nullptr) {
+            return 0;
+        }
+        
+        num = num * 10 + node->val;
+        
+        if (node->left == nullptr && node->right == nullptr) {
+            return num;
+        }
+        
+        return dfs(node->left, num) + dfs(node->right, num);
+    }    
+};
