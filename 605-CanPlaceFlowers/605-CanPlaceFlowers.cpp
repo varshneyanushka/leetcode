728fcaee8224c80e654d04f1ac2b06b83780798e
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:57:28 +0530

    Create 605-CanPlaceFlowers

---
+++ 605-CanPlaceFlowers/605-CanPlaceFlowers.cpp
@@ -0,0 +1,27 @@
+// Last updated: 07/11/2025, 19:57:27
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& f, int n) {
+        int ans=0;
+        f.insert(f.begin(),0);
+        f.push_back(0);
+        for(int i=1;i<f.size()-1;i++)
+        {
+            
+            if(f[i-1]==0 && f[i]==0 && f[i+1]==0)
+            {
+                f[i]=1;
+                ans++;
+            }
+            
+        }
+        if(ans>=n){
+                return true;
+            }
+            else{
+                     return false;
+            }
+        
+       
+    }
+};
