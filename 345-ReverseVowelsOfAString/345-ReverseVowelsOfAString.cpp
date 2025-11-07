34cb1ac5d22e09935237bdf843cd581f8c6e9790
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:57:41 +0530

    Create 345-ReverseVowelsOfAString

---
+++ 345-ReverseVowelsOfAString/345-ReverseVowelsOfAString.cpp
@@ -0,0 +1,26 @@
+// Last updated: 07/11/2025, 19:57:41
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<char> temp;
+        for(int i=0;i<s.length();i++)
+        {
+            if(tolower(s[i])=='a'||tolower(s[i])=='e'||tolower(s[i])=='i'||tolower(s[i])=='o'||tolower(s[i])=='u')
+            {
+                temp.push_back(s[i]);
+            }
+        }
+        for(int i=0;i<s.length();i++){
+            if(tolower(s[i])=='a'||tolower(s[i])=='e'||tolower(s[i])=='i'||tolower(s[i])=='o'||tolower(s[i])=='u')
+            {
+                char t;
+                t=temp.back();
+                temp.pop_back();
+                s[i]=t;
+            }
+
+        }
+        return s;
+
+    }
+};
