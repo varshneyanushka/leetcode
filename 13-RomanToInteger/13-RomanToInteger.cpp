3e676e5773967b1144611acf8104cd4d5b9216c8
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:59:17 +0530

    Create 13-RomanToInteger

---
+++ 13-RomanToInteger/13-RomanToInteger.cpp
@@ -0,0 +1,32 @@
+// Last updated: 07/11/2025, 19:59:16
+class Solution {
+public:
+    int romanToInt(string s) {
+       map<char, int> map = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
+    };
+    int n =s.length();
+    int ans=0;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(map[s[i]]<map[s[i+1]])
+        {
+            ans=ans-map[s[i]];
+        }
+        else
+        {
+            ans=ans+map[s[i]];
+        }
+
+    }
+        return ans;
+        
+    }
+};
