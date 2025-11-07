21735ae400c4547229c42b54792219e344667684
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:59:13 +0530

    Create 17-LetterCombinationsOfAPhoneNumber

---
+++ 17-LetterCombinationsOfAPhoneNumber/17-LetterCombinationsOfAPhoneNumber.cpp
@@ -0,0 +1,34 @@
+// Last updated: 07/11/2025, 19:59:12
+class Solution {
+    private:
+   unordered_map<char, string> map = {
+            {'2', "abc"}, {'3', "def"}, {'4', "ghi"},
+            {'5', "jkl"}, {'6', "mno"}, {'7', "pqrs"},
+            {'8', "tuv"}, {'9', "wxyz"}
+        };
+        vector<string> ans;
+public:
+     void backtrack(int i, string curr,string d) 
+        {
+            if (curr.length() == d.length()) 
+            {
+                ans.push_back(curr);
+                return;
+            }
+            for (char c : map[d[i]]) 
+            {
+                backtrack(i + 1, curr + c,d);
+            }
+        }
+    vector<string> letterCombinations(string d) 
+    {
+       if(d.empty()) return {};
+        int n=d.length();
+
+        backtrack(0,"",d);
+       
+        return ans;
+        
+        
+    }
+};
