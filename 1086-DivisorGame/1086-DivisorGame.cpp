f021310924f81ab2673a5a05575bf8789f125f83
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:57:07 +0530

    Create 1086-DivisorGame

---
+++ 1086-DivisorGame/1086-DivisorGame.cpp
@@ -0,0 +1,11 @@
+// Last updated: 07/11/2025, 19:57:06
+class Solution {
+public:
+    bool divisorGame(int n) {
+        if (n%2==0)
+        {
+            return true;
+        }
+        return false;
+    }
+};
