724fc819a57ed9967c9340523e0f104048efeec6
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:58:45 +0530

    Create 88-MergeSortedArray

---
+++ 88-MergeSortedArray/88-MergeSortedArray.cpp
@@ -0,0 +1,14 @@
+// Last updated: 07/11/2025, 19:58:44
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int j=m;
+       for(int i=0;i<n;i++)
+       {
+        nums1[j]=nums2[i];
+        j++;
+       }
+       sort(nums1.begin(),nums1.end());
+       return;
+    }
+};
