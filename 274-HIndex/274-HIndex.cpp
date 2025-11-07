c7ccde854bd062eb7f1c5bc937ee071f3335aa77
Anushka Varshney <134060399+varshneyanushka@users.noreply.github.com>
2025-11-07 19:57:51 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.cpp
@@ -0,0 +1,41 @@
+// Last updated: 07/11/2025, 19:57:51
+class Solution {
+public:
+    int hIndex(vector<int> c) {
+        int h=0;
+        int n=c.size()+1;
+        if(n==1 )
+        {
+            if(c[0]==0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        vector<int> dp(n,0);
+        sort(c.begin(),c.end(),greater<int>());
+        for(int d:c)
+        {
+            for(int i=1;i<n;i++)
+        {
+            if(d>=i)
+            {
+                dp[i]++;
+            }
+        }
+        }
+        for(int i=1;i<n;i++)
+        {
+            if(dp[i]>=i)
+            {
+                h=max(h,i);
+            }
+        }
+        
+        
+        return h;
+    }
+};
